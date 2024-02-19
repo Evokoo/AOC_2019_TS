30db2ca83f6433fd0c46db500026185c38cb0646
Evokoo <evanjohncooper@gmail.com>
2024-02-20 02:29:24 +1100

    Day 02 complete

--- 02/02.test.ts
+++ 02/02.test.ts
@@ -7,21 +7,21 @@ const currentDay = path.basename(__dirname);
 describe(`AOC 2018 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		test("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(null);
+			expect(solveA("example_a", currentDay)).toBe(3500);
 		});
 
 		test("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(null);
+			expect(solveA("input", currentDay)).toBe(2782414);
 		});
 	});
 
 	describe("Part B", () => {
-		test("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(null);
-		});
+		// test("Example", () => {
+		// 	expect(solveB("example_b", currentDay)).toBe(null);
+		// });
 
 		test("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(null);
+			expect(solveB("input", currentDay)).toBe(9820);
 		});
 	});
 });
--- 02/02.ts
+++ 02/02.ts
@@ -3,15 +3,67 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		register = parseIntput(data),
+		value = runIntCodes(register);
+
+	return value;
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		value = findPair(data, 19690720);
+
+	return value;
 }
 
 //Run
-solveA("example_a", "02");
+solveB("input", "02");
 
 // Functions
+function parseIntput(data: string) {
+	const register = (data.match(/\d+/g) || []).map(Number);
+
+	if (register.length > 15) {
+		register[1] = 12;
+		register[2] = 2;
+	}
+
+	return register;
+}
+function runIntCodes(register: number[]) {
+	for (let i = 0; i < register.length; i += 4) {
+		const code = register[i];
+
+		if (code === 1) {
+			const [a, b, c] = register.slice(i + 1, i + 4);
+			register[c] = register[a] + register[b];
+		}
+
+		if (code === 2) {
+			const [a, b, c] = register.slice(i + 1, i + 4);
+			register[c] = register[a] * register[b];
+		}
+
+		if (code === 99) {
+			break;
+		}
+	}
+
+	return register[0];
+}
+function findPair(data: string, target: number) {
+	for (let noun = 0; noun < 99; noun++) {
+		for (let verb = 0; verb < 99; verb++) {
+			const register = parseIntput(data);
+
+			register[1] = noun;
+			register[2] = verb;
+
+			if (runIntCodes(register) === target) {
+				return 100 * noun + verb;
+			}
+		}
+	}
+
+	throw Error("Value not found");
+}
