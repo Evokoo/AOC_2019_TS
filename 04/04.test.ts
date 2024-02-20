9185f74b1ebe3d7bb26b14eee4257e3b4d0f3e6c
Evokoo <evanjohncooper@gmail.com>
2024-02-21 01:00:02 +1100

    Day 04 complete

--- 04/04.test.ts
+++ 04/04.test.ts
@@ -7,21 +7,21 @@ const currentDay = path.basename(__dirname);
 describe(`AOC 2018 - Day ${currentDay}`, () => {
 	describe("Part A", () => {
 		test("Example", () => {
-			expect(solveA("example_a", currentDay)).toBe(null);
+			expect(solveA("example_a", currentDay)).toBe(1231);
 		});
 
 		test("Solution", () => {
-			expect(solveA("input", currentDay)).toBe(null);
+			expect(solveA("input", currentDay)).toBe(1873);
 		});
 	});
 
 	describe("Part B", () => {
 		test("Example", () => {
-			expect(solveB("example_b", currentDay)).toBe(null);
+			expect(solveB("example_b", currentDay)).toBe(898);
 		});
 
 		test("Solution", () => {
-			expect(solveB("input", currentDay)).toBe(null);
+			expect(solveB("input", currentDay)).toBe(1264);
 		});
 	});
 });
--- 04/04.ts
+++ 04/04.ts
@@ -3,15 +3,60 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		{ lower, upper } = parseInput(data),
+		passwords = findPasswords(lower, upper);
+
+	return passwords;
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
-	return 0;
+	const data = TOOLS.readData(fileName, day),
+		{ lower, upper } = parseInput(data),
+		passwords = findPasswords(lower, upper, true);
+
+	console.log(passwords);
+
+	return passwords;
 }
 
 //Run
-solveA("example_a", "04");
+solveB("input", "04");
 
 // Functions
+function parseInput(data: string) {
+	const [lower, upper] = data.split("-").map(Number);
+	return { lower, upper };
+}
+function findPasswords(lower: number, upper: number, partB: boolean = false) {
+	function hasDouble(num: number) {
+		const pairs = String(num).match(/(\d)\1{1,}/g) || [];
+
+		if (!pairs.length) {
+			return false;
+		} else {
+			return partB ? pairs.some((pair) => pair.length === 2) : true;
+		}
+	}
+	function isIncreasing(num: number) {
+		const digits = [...String(num)].map(Number);
+
+		for (let i = 1; i < digits.length; i++) {
+			const [a, b] = [digits[i - 1], digits[i]];
+
+			if (b >= a) continue;
+			else return false;
+		}
+
+		return true;
+	}
+
+	const passwords: Set<number> = new Set();
+
+	for (let i = lower; i < upper; i++) {
+		if (hasDouble(i) && isIncreasing(i)) {
+			passwords.add(i);
+		}
+	}
+
+	return passwords.size;
+}
