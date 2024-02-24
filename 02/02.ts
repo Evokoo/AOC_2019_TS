325c6419a72f20c70caeebda12d250cebd4c28eb
Evokoo <evanjohncooper@gmail.com>
2024-02-25 01:04:00 +1100

    Day 02 refactored to use Intcode class

--- 02/02.ts
+++ 02/02.ts
@@ -1,69 +1,40 @@
 // Imports
 import TOOLS from "../00/tools";
+import Intcode from "../Intcode";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day),
-		register = parseIntput(data),
-		value = runIntCodes(register);
+		intComp = new Intcode(data);
 
-	return value;
+	if (intComp.register.length > 15) {
+		intComp.setRegisterValue(1, 12);
+		intComp.setRegisterValue(2, 2);
+	}
+
+	intComp.run();
+
+	return intComp.getRegisterValue(0);
 }
 export function solveB(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day),
-		value = findPair(data, 19690720);
+	const data = TOOLS.readData(fileName, day);
 
-	return value;
-}
-
-//Run
-solveB("input", "02");
-
-// Functions
-function parseIntput(data: string) {
-	const register = (data.match(/\d+/g) || []).map(Number);
-
-	if (register.length > 15) {
-		register[1] = 12;
-		register[2] = 2;
-	}
-
-	return register;
-}
-function runIntCodes(register: number[]) {
-	for (let i = 0; i < register.length; i += 4) {
-		const code = register[i];
-
-		if (code === 1) {
-			const [a, b, c] = register.slice(i + 1, i + 4);
-			register[c] = register[a] + register[b];
-		}
-
-		if (code === 2) {
-			const [a, b, c] = register.slice(i + 1, i + 4);
-			register[c] = register[a] * register[b];
-		}
-
-		if (code === 99) {
-			break;
-		}
-	}
-
-	return register[0];
-}
-function findPair(data: string, target: number) {
 	for (let noun = 0; noun < 99; noun++) {
 		for (let verb = 0; verb < 99; verb++) {
-			const register = parseIntput(data);
+			const intComp = new Intcode(data);
 
-			register[1] = noun;
-			register[2] = verb;
+			intComp.setRegisterValue(1, noun);
+			intComp.setRegisterValue(2, verb);
+			intComp.run();
 
-			if (runIntCodes(register) === target) {
+			if (intComp.getRegisterValue(0) === 19690720) {
 				return 100 * noun + verb;
 			}
 		}
 	}
 
-	throw Error("Value not found");
+	throw Error("Pair not found");
 }
+
+//Run
+solveB("input", "02");
