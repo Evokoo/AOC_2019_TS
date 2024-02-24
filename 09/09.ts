30dd7bd5d732e7a640b3d9a6f5cd1f1b15a18b78
Evokoo <evanjohncooper@gmail.com>
2024-02-25 03:48:42 +1100

    Files: added example data

--- 09/09.ts
+++ 09/09.ts
@@ -1,9 +1,15 @@
 // Imports
 import TOOLS from "../00/tools";
+import Intcode from "../Intcode";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
+	const data = TOOLS.readData(fileName, day),
+		intComp = new Intcode(data);
+
+	intComp.run();
+	console.log(intComp.outputs);
+
 	return 0;
 }
 export function solveB(fileName: string, day: string): number {
