eb02729997a64d11b54698335718341c696e6d95
Evokoo <evanjohncooper@gmail.com>
2024-02-25 02:39:28 +1100

    Refactor: Changed register to Map from Array

--- Intcode.ts
+++ Intcode.ts
@@ -1,9 +1,13 @@
+type Register = Map<number, number>;
+
 export default class Intcode {
-	private _register: number[];
+	// private _register: number[];
+	private _register: Register;
 	private _pointer: number;
 	private _inputs: number[];
 	private _outputs: number[];
 	private _active: boolean;
+	private _relativeBase: number;
 
 	constructor(data: string, inputs?: number[]) {
 		this._register = this.initRegister(data);
@@ -11,16 +15,25 @@ export default class Intcode {
 		this._pointer = 0;
 		this._outputs = [];
 		this._active = true;
+		this._relativeBase = 0;
 	}
 
 	// Intcode setup
-	private initRegister = (data: string): number[] => {
-		return data.split(",").map(Number);
+	private initRegister = (data: string): Register => {
+		// return data.split(",").map(Number);
+		const register: Register = new Map();
+		const values = data.split(",").map(Number);
+
+		for (let i = 0; i < values.length; i++) {
+			register.set(i, values[i]);
+		}
+
+		return register;
 	};
 
 	// Intcode register value processing
 	private analyseRegisterValue = () => {
-		const registerValue = Math.abs(this._register[this._pointer]);
+		const registerValue = Math.abs(this.getRegisterValue(this._pointer));
 		const paddedValue = String(registerValue).padStart(5, "0");
 
 		return {
@@ -29,62 +42,93 @@ export default class Intcode {
 		};
 	};
 	private getVariables = (code: number, modes: string[]): number[] => {
-		let varibles: number[];
+		let varibles: number[] = [];
 
 		switch (code) {
 			case 1:
 			case 2:
 			case 7:
 			case 8:
-				varibles = this._register.slice(this._pointer + 1, this._pointer + 4);
+				varibles = [
+					this.getRegisterValue(this._pointer + 1),
+					this.getRegisterValue(this._pointer + 2),
+					this.getRegisterValue(this._pointer + 3),
+				];
 				break;
 			case 3:
 			case 4:
-				varibles = [this._register[this._pointer + 1]];
+			case 9:
+				varibles = [this.getRegisterValue(this._pointer + 1)];
 				break;
 			case 5:
 			case 6:
 				varibles = [
-					this.register[this._pointer + 1],
-					this.register[this._pointer + 2],
+					this.getRegisterValue(this._pointer + 1),
+					this.getRegisterValue(this._pointer + 2),
 				];
 				break;
 			default:
 				return [];
 		}
 
-		if (varibles.length === 1) {
-			return varibles;
-		} else {
-			return varibles.map((val, index) => {
-				if (varibles.length === 3) {
-					return index < varibles.length - 1 && modes[index] === "0"
-						? this.register[val]
-						: val;
-				} else {
-					return modes[index] === "0" ? this.register[val] : val;
-				}
-			});
+		let offset = 0;
+
+		if ([1, 2, 3, 4, 7, 8, 9].includes(code)) {
+			offset = 1;
 		}
+
+		for (let i = 0; i < varibles.length - offset; i++) {
+			const mode = +modes[i];
+			const varible = varibles[i];
+
+			switch (mode) {
+				case 0:
+					varibles[i] = this.getRegisterValue(varible);
+					break;
+				case 1:
+					break;
+				case 2:
+					let pointer = varible + this._relativeBase;
+					let registerSize = this._register.size;
+
+					if (pointer > registerSize) {
+						pointer = pointer % registerSize;
+					}
+
+					if (pointer < 0) {
+						pointer = (pointer + registerSize) % registerSize;
+					}
+
+					varibles[i] = pointer;
+
+					break;
+				default:
+					throw Error("Invalid mode");
+			}
+		}
+
+		return varibles;
 	};
 
 	// Intcode numbered methods
 	private addValues = (varibles: number[]): void => {
-		this._register[varibles[2]] = varibles[0] + varibles[1];
+		this.setRegisterValue(varibles[2], varibles[0] + varibles[1]);
 	};
 	private multiplyValues = (varibles: number[]): void => {
-		this._register[varibles[2]] = varibles[0] * varibles[1];
+		this.setRegisterValue(varibles[2], varibles[0] * varibles[1]);
 	};
 	private inputValue = (varibles: number[]): boolean => {
 		if (this._inputs.length) {
-			this._register[varibles[0]] = this._inputs.shift()!;
+			this.setRegisterValue(varibles[0], this._inputs.shift()!);
+			// this._register[varibles[0]] = this._inputs.shift()!;
 			return false;
 		} else {
 			return true;
 		}
 	};
 	private outputValue = (varibles: number[]): void => {
-		this._outputs.push(this.register[varibles[0]]);
+		this._outputs.push(this.getRegisterValue(varibles[0]));
+		// this._outputs.push(this.register[varibles[0]]);
 	};
 	private isNotZero = (varibles: number[]): boolean => {
 		if (varibles[0] !== 0) {
@@ -103,10 +147,13 @@ export default class Intcode {
 		}
 	};
 	private isLessThan = (varibles: number[]): void => {
-		this._register[varibles[2]] = varibles[0] < varibles[1] ? 1 : 0;
+		this.setRegisterValue(varibles[2], varibles[0] < varibles[1] ? 1 : 0);
 	};
 	private isEqual = (varibles: number[]): void => {
-		this._register[varibles[2]] = varibles[0] === varibles[1] ? 1 : 0;
+		this.setRegisterValue(varibles[2], varibles[0] === varibles[1] ? 1 : 0);
+	};
+	private updateRelativeValue = (varibles: number[]): void => {
+		this._relativeBase += varibles[0];
 	};
 
 	//PUBLIC METHODS
@@ -116,20 +163,20 @@ export default class Intcode {
 		this._inputs.push(value);
 	};
 	public setRegisterValue = (pointer: number, value: number) => {
-		this._register[pointer] = value;
+		if (pointer < 0) throw RangeError("Pointer cannot be negative");
+		this._register.set(pointer, value);
 	};
 	public getRegisterValue = (pointer: number) => {
-		if (!this._register[pointer]) {
-			throw RangeError("Value not found at the specified pointer");
-		}
-
-		return this._register[pointer];
+		if (pointer < 0) throw RangeError("Pointer cannot be negative");
+		return this._register.get(pointer) ?? 0;
 	};
 
 	// Run Intocde class
 	public run = () => {
 		const { code, modes } = this.analyseRegisterValue();
 		const varibles = this.getVariables(code, modes);
+
+		console.log(this._relativeBase);
 
 		let jump = false;
 
@@ -162,6 +209,8 @@ export default class Intcode {
 			case 8:
 				this.isEqual(varibles);
 				break;
+			case 9:
+				this.updateRelativeValue(varibles);
 			default:
 				break;
 		}
@@ -176,11 +225,11 @@ export default class Intcode {
 	//GETTERS & SETTERS
 
 	// Get Registers
-	get register(): number[] {
+	get register(): Register {
 		return this._register;
 	}
 	// Get Outputs
-	get ouputs(): number[] {
+	get outputs(): number[] {
 		return this._outputs;
 	}
 	// Get last output
