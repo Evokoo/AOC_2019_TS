c3626bb72915c3e495f0b9769b957c18b8a2750a
Evokoo <evanjohncooper@gmail.com>
2024-02-28 00:51:56 +1100

    Day 10 complete

--- 10/10.ts
+++ 10/10.ts
@@ -5,26 +5,28 @@ import TOOLS from "../00/tools";
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day),
 		astroidMap = parseInput(data),
-		{ location, visible } = locateStation(astroidMap);
+		{ visible } = locateStation(astroidMap);
 
 	return visible;
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day),
 		astroidMap = parseInput(data),
-		{ location, visible } = locateStation(astroidMap);
+		{ astroidList } = locateStation(astroidMap),
+		{ position } = astroidList.sort((a, b) => a.bearing - b.bearing)[199];
 
-	return visible;
+	return position.x * 100 + position.y;
 }
 
 //Run
-solveB("example_b", "10");
+solveB("input", "10");
 
 // Functions
 type Point = { x: number; y: number };
 type Grid = string[][];
 type Dimensions = { maxX: number; maxY: number };
 type AstroidMap = { grid: Grid; dimensions: Dimensions };
+type Astroid = { position: Point; bearing: number };
 
 function parseInput(data: string) {
 	const grid = data.split("\r\n").map((row) => [...row]);
@@ -36,24 +38,27 @@ function parseInput(data: string) {
 function locateStation(astroidMap: AstroidMap) {
 	let visible = 0;
 	let location = { x: 0, y: 0 };
+	let astroidList: Astroid[] = [];
 
 	for (let y = 0; y < astroidMap.dimensions.maxY; y++) {
 		for (let x = 0; x < astroidMap.dimensions.maxX; x++) {
 			if (astroidMap.grid[y][x] === "#") {
-				const count = lineOfSight({ x, y }, astroidMap);
+				const { count, list } = lineOfSight({ x, y }, astroidMap);
 
 				if (count > visible) {
 					visible = count;
 					location = { x, y };
+					astroidList = list;
 				}
 			}
 		}
 	}
 
-	return { location, visible };
+	return { location, visible, astroidList };
 }
 function lineOfSight(astroid: Point, astroidMap: AstroidMap) {
 	const visble: Set<number> = new Set();
+	const astroids: Astroid[] = [];
 
 	for (let y = 0; y < astroidMap.dimensions.maxY; y++) {
 		for (let x = 0; x < astroidMap.dimensions.maxX; x++) {
@@ -62,13 +67,18 @@ function lineOfSight(astroid: Point, astroidMap: AstroidMap) {
 				!(astroid.y === y && astroid.x === x)
 			) {
 				const angle = Math.atan2(y - astroid.y, x - astroid.x);
-				visble.add(angle);
+				let degrees = (angle * (180 / Math.PI) + 360) % 360;
+				degrees = (degrees + 90) % 360;
+
+				if (visble.has(degrees)) {
+					continue;
+				} else {
+					visble.add(degrees);
+					astroids.push({ position: { x, y }, bearing: degrees });
+				}
 			}
 		}
 	}
 
-	return visble.size;
-
-	// console.log({ astroid, visible, count: visible.length });
+	return { count: visble.size, list: astroids };
 }
-function destroyAstroids(astroid: P);
