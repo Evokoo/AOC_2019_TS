64b54a8c754caa482f425ffd09cdc2a8a8854dd3
Evokoo <evanjohncooper@gmail.com>
2024-03-01 01:34:32 +1100

    Day 14-1 WIP

--- 14/14.ts
+++ 14/14.ts
@@ -3,7 +3,10 @@ import TOOLS from "../00/tools";
 
 //Solutions
 export function solveA(fileName: string, day: string): number {
-	const data = TOOLS.readData(fileName, day);
+	const data = TOOLS.readData(fileName, day),
+		recipes = parseInput(data);
+
+	calculateOre(recipes);
 	return 0;
 }
 export function solveB(fileName: string, day: string): number {
@@ -15,3 +18,59 @@ export function solveB(fileName: string, day: string): number {
 solveA("example_a", "14");
 
 // Functions
+type Recipe = {
+	quantity: number;
+	inputs: {
+		[ID: string]: number;
+	};
+};
+type Recipes = {
+	[ID: string]: Recipe;
+};
+
+function parseInput(data: string) {
+	const recipes: Recipes = {};
+
+	for (let line of data.split("\r\n")) {
+		const recipe: Recipe = {
+			quantity: 0,
+			inputs: {},
+		};
+
+		const [inputs, output] = line.split(" => ");
+		const [outputQuantity, outputChemical] = output.split(" ");
+
+		recipe.quantity = +outputQuantity;
+
+		for (let input of inputs.split(", ")) {
+			const [inputQuantity, inputChemical] = input.split(" ");
+			recipe.inputs[inputChemical] = +inputQuantity;
+		}
+
+		recipes[outputChemical] = recipe;
+	}
+
+	return recipes;
+}
+function calculateOre(recipes: Recipes) {
+	const queue: Recipe[] = [recipes["FUEL"]];
+	const required: Record<string, number> = {};
+
+	let ore: number = 0;
+
+	while (queue.length) {
+		const current = queue.pop()!;
+
+		for (let [chemical, quantity] of Object.entries(current.inputs)) {
+			if (chemical === "ORE") continue;
+
+			required[chemical] = (required[chemical] ?? 0) + quantity;
+
+			const units = Math.ceil(quantity / recipes[chemical].quantity);
+
+			for (let i = 0; i < units; i++) {
+				queue.push(recipes[chemical]);
+			}
+		}
+	}
+}
