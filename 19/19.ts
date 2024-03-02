bfa62b106e0f29d6e740dc3e876dcaa2a4a8b02e
Evokoo <evanjohncooper@gmail.com>
2024-03-03 02:54:01 +1100

    Day 19 complete

--- 19/19.ts
+++ 19/19.ts
@@ -5,31 +5,49 @@ import Intcode from "../Intcode";
 //Solutions
 export function solveA(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	runIntcode(data, 50);
-	return 0;
+	return getBeamSize(data, 50);
 }
 export function solveB(fileName: string, day: string): number {
 	const data = TOOLS.readData(fileName, day);
-	return 0;
+	return locateShip(data, 100);
 }
 
 //Run
-solveA("input", "19");
+solveB("input", "19");
 
 // Functions
-function runIntcode(data: string, size: number) {
+function inBeam(data: string, x: number, y: number) {
+	const comp = new Intcode(data, [x, y]);
+	comp.run();
+	return comp.lastOutput === 1;
+}
+function getBeamSize(data: string, size: number) {
 	const beam: Set<string> = new Set();
 
 	for (let y = 0; y < size; y++) {
 		for (let x = 0; x < size; x++) {
-			const comp = new Intcode(data, [x, y]);
-			comp.run();
-
-			if (comp.lastOutput === 1) {
+			if (inBeam(data, x, y)) {
 				beam.add(`${x},${y}`);
 			}
 		}
 	}
 
-	console.log(beam.size);
+	return beam.size;
+}
+function locateShip(data: string, size: number) {
+	const offset = size - 1;
+
+	for (let y = size; true; y++) {
+		for (let x = ~~(y / 1.5); true; x++) {
+			if (!inBeam(data, x, y)) {
+				continue;
+			}
+
+			if (inBeam(data, x, y) && inBeam(data, x + offset, y - offset)) {
+				return x * 10000 + (y - offset);
+			} else {
+				break;
+			}
+		}
+	}
 }
